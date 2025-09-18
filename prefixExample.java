28ab6e13d3ffc9de3cab350c5b4278d4838a37bb
Deepika.N <deepika.n2607@gmail.com>
2025-09-18 20:30:43 +0530

    Add files via upload

---
+++ prefixExample.java
@@ -0,0 +1,10 @@
+public class prefixExample {
+    public static void main(String[]args){
+        int a=5,b=3;
+        int result=++a*b+2;
+        System.out.println("Result:"+result);
+    }
+}
+
+    
+
